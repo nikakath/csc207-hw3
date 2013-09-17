5ab57ebceb1c2f5d9696afd19bbd40bf51a5347b
KittNika <kittnika@gmail.com>
2013-09-16 21:57:18 -0500

    Last Update?

--- src/edu/grinnell/csc207/nikakath/utils/Calculator.java
+++ src/edu/grinnell/csc207/nikakath/utils/Calculator.java
@@ -1,6 +1,10 @@
 package edu.grinnell.csc207.nikakath.utils;
 
 public class Calculator {
+	
+	/*
+	 * Author: Kitt Nika
+	 */
 
 	public static void main(String[] args) throws Exception {
 
@@ -8,20 +12,73 @@ public class Calculator {
 		pen = new java.io.PrintWriter(System.out, true);
 		
 		pen.println("FIRST TEST");
-		for (int i = 0; i < (fewestCoins(28)).length; i++) {
+		for (int i = 0; i < 4; i++) {
 		    pen.print('\n' + (fewestCoins(28))[i] + " ");
 		} // for
-		pen.println("SECOND TEST");
-		for (int i = 0; i < (fewestCoins(20)).length; i++) {
+		pen.println("\nSECOND TEST");
+		for (int i = 0; i < 4; i++) {
 		    pen.print('\n' + (fewestCoins(20))[i] + " ");
 		} // for
-		// pen.println(28%54);
 		
 	}
 
+	/*
+	 * Preconditions: Takes a string representing a simple calculation that can invoke the +, -, *, /, and ^ operators.
+	 * Postconditions: Returns the correct answer to the calculation provided, as an integer. (Incomplete)
+	 */
+	
+	public static int calculator(String calc) {
+		return 0;
+	} //calculator(String)
+	
+	public static boolean isMultiple(long a, long b){
+		if(a == 0)
+			return true;
+		if(b == 0)
+			return false;
+		return (a%b == 0);
+	} //isMultiple(long,long)
+	
+	/*
+	 * Preconditions: No additional.
+	 * Postconditions: Returns an array of integers, the values representing the number of each coin required 
+	 * to make the necessary change, so that fewest[0] is the number of wots needed, fewest[1] is the
+	 * number of eaters, etc.
+	 */
+	
 	public static int[] fewestCoins(int i) {
-		int[] fewest = new int[20];		
+		int[] fewest = {0, 0, 0, 0};
+		if (isMultiple((long) i, 54)){
+			fewest[3] = (i / 54);
+			return fewest;
+		}
+		
+		if (isMultiple((long) i, 11)){
+			fewest[2] = (i / 11);
+			return fewest;
+		}
+		
+		if (isMultiple((long) i, 7)){
+			fewest[1] = (i / 7);
+			return fewest;
+		}
+		
+		if (isMultiple((long) i, 2)){
+			fewest[0] = (i / 2);
+			return fewest;
+		}
+		
+		
+		
 		return fewest;
 		
-	} //fewestCoins
-}
+	} //fewestCoins(int)
+
+	/*
+	 * Citations: Method isMultiple taken from csc207-hw2 Java package, by Fiona Byrne, 
+	 * 			  Justus Goldstein-Shirley, and Kitt Nika found at:
+	 * 					https://github.com/nikakath/csc207-hw2
+	 */
+
+}//Calculator
+
--- src/edu/grinnell/csc207/nikakath/utils/StringUtils.java
+++ src/edu/grinnell/csc207/nikakath/utils/StringUtils.java
@@ -3,6 +3,10 @@ package edu.grinnell.csc207.nikakath.utils;
 import java.lang.reflect.Array;
 
 public class StringUtils {
+	
+	/*
+	 * Author: Kitt Nika
+	 */
 
 	public static void main(String[] args) throws Exception {
 
@@ -20,28 +24,49 @@ public class StringUtils {
 		
 	}
 
+	/* Preconditions: Takes a string of fields that are separated by a character, ch,
+	 * which is also inputed.
+	 * Postconditions: Returns an array of strings including the fields with the
+	 * separation character removed. (Incomplete)
+	 */
+	
 	public static String[] splitAt(String str, char ch) {
-		int i, j, k;
+		int start, end, k;
 		k = 0;
-		j = 0;
+		end = 0;
 		String[] split = new String[str.length()];
 		
-		for (i = 0; i <= str.length(); i++){
-			if (str.charAt(i) != ch){
-				split[k] = str.substring(i, j);
+		for (start = 0; start <= str.length(); start++){
+			// When you hit the dividing character, place the section before it into the
+			//  array to be returned and update the counts and place holders
+			if (str.charAt(start) != ch){
+				split[k] = str.substring(start, end);
 				k++;
-				j = i;
+				end = start;
 				} //if
 			
 			} //for
 		
 		return split;
 		
-	} //splitAt
+	} //splitAt(String, char)
+	
+	/* Preconditions: Takes a string of fields that are separated by commas and returns an array of strings
+	 * containing the fields.  If a comma occurs between two quotation marks, it is left alone.  If a quote
+	 * appears between two quotation marks, it is treated as a single character and is left alone.
+	 * Postconditions: Returns an array of strings including the fields with the commas removed. (Incomplete)
+	 */
 	
 	public static String[] splitCVS(String str, char ch) {
-		
-	} //splitCVS
+		String[] split = new String[str.length()];
+		return split;
+	} //splitCVS(String, char)
+	
+	/* Preconditions: Nothing notable.
+	 * Postconditions: Returns a "deleeted" version of the input string,
+	 * where symbols representing letters in the 133+ alphabet are
+	 * replaced by their true English alphabetic signifiers.
+	 */
 	
 	public static String deLeet(String str){
 		String deLeeted = str;
@@ -53,24 +78,35 @@ public class StringUtils {
 		String deLeeted6 = deLeeted5.replace("+", "t");
 		String deLeeted7 = deLeeted6.replace("3", "e");
 		return deLeeted7;
-	} //deLeet
+	} //deLeet(String)
+	
+	/* Preconditions: Takes a string representing a name.  Does not work if the name starts with a vowel or 'y'.
+	 * Postconditions: Prints a silly verse with the original name inserted and modified in different ways.
+	 */
 	
 	public static void nameGame(String name){
 		int i;
 		
+		// Find the index at which the first vowel occurs
 		for (i = 0; i < name.length(); i++){
 			if (name.charAt(i) == 'a' || name.charAt(i) == 'e' || name.charAt(i) == 'i' || name.charAt(i) == 'o' ||
 					name.charAt(i) == 'u' || name.charAt(i) == 'y'){
 				break;
 				}
 			}
-		String newname = name.substring(i);		
+		
+		// String to insert into appropriate places in verse
+		String newname = name.substring(i);
+		
+		// Set up something to print with		
 		java.io.PrintWriter pen;
 		pen = new java.io.PrintWriter(System.out, true);
+		
+		// Print the verse
 		pen.println(name + "!");
 		pen.println(name + ", " + name + " bo B" + newname + " Bonana fanna fo F" + newname);
 		pen.println("Fee fy mo M" + newname + ", " + name + "!");
 		
-	} //nameGame
+	} //nameGame(String)
 	
 } //StringUtils
